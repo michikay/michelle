c2280f417d9eb3cbd460db28732a0ffb2419438b
michikay <41812237+michikay@users.noreply.github.com>
2018-07-28 12:40:54 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,85 @@
+<h1>My Personal Information<h1>
+
+<img src="images/1.jpg" style ="float:left;height="20%" width="15%">
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<h2>Name	: Michelle P. Ombid</h2>
+
+<h3>Age		: 26</h3>
+
+<h4>B-day	: May 6, 1992</h4>
+
+<h5>Genger	: Female</h5>
+
+<h6>Address	: Brgy.08 Housing Project Guiuan, Eastern Samar</h6>
+
+
+
+<h3>GOALS</h3>
+
+<ul>
+
+<li>
+
+	My goals in life is to finish my masteral degree  so that when I go back to 
+
+	my teaching career I will be able to give a quality education to my students.
+
+	And after I finished my masteral degree, I will pursue my doctorate.
+
+
+
+</li>
+
+</ul>
+
+
+
+<h3>OBJECTIVE</h3>
+
+<br>
+
+	To pass all my subjects
+
+	To pass my comprehensive examination
+
+	To finish my masteral degree
+
+	To go back in teaching with full knowledge
+
+</li>
+
+</ul>
+
+
+
+<h3>About Me</h3>	
+
+<br>
+
+	I am a simple woman driven my life to be a successful individual,
+
+	I love singing and dancing, which amuse me during my boredom, I want 
+
+	to travel the world, and enjoy life to the fullest. I love doing things on my own
+
+	but I love it even more when I do things with my bestfriends.
+
+</li>
+
+</ul>
+
+
+
+</body>
+
+</html>
